4303c472779d09311a9c84c499378f348bfecf72
malak4822 <kmalak4822@gmail.com>
2021-12-11 13:38:21 +0100

    login pag, nie zrobiona jeszcze

--- lib/main.dart
+++ lib/main.dart
@@ -11,48 +11,122 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: true,
+      title: 'Poppy App',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Container(
-        color: Colors.black,
+        color: Colors.greenAccent,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                "Joł byku!",
-                style:
-                    GoogleFonts.overpass(color: Colors.white, fontSize: 40.0),
+              Image.asset(
+                "img/logo.png",
+                width: 150.0,
+                height: 150.0,
               ),
-              Text(
-                '$_counter',
-                style:
-                    GoogleFonts.overpass(color: Colors.white, fontSize: 30.0),
+              Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1.5),
+                      color: Colors.white,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Icon(Icons.email_outlined, size: 40.0),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          child: TextFormField(
+                            maxLines: 1,
+                            decoration: const InputDecoration(
+                              label: Text(" E-mail"),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1.5),
+                      color: Colors.white,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Icon(Icons.vpn_key, size: 40.0),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          child: TextFormField(
+                            maxLines: 1,
+                            decoration: const InputDecoration(
+                              label: Text(" Password"),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    onPrimary: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Ink(
+                  decoration: BoxDecoration(
+                      gradient: const LinearGradient(colors: [
+                        Colors.lightBlue,
+                        Colors.white30,
+                        Colors.lightBlue,
+                      ]),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Container(
+                    width: 200,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: Text('Zaloguj się',
+                        style: GoogleFonts.overpass(
+                            color: Colors.greenAccent, fontSize: 40.0)),
+                  ),
+                ),
               ),
             ],
           ),
